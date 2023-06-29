da063ab4e48ee947488c1dd7bdcf623ae1297d58
Khushi Shukla <khushishukla5890@gmail.com>
2023-06-29 22:24:02 +0530

    added production setting

--- elevator_problem/settings/production.py
+++ elevator_problem/settings/production.py
@@ -1,3 +1,14 @@
 from .base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': 'containers-us-west-188.railway.app',
+        'PORT': '7818',
+    }
+}
 
+ALLOWED_HOSTS = ['localhost', 'containers-us-west-188.railway.app']
